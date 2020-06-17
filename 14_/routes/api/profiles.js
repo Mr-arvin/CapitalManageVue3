4db5f9aed984d201ccb5788594ca0741638cb8ad
xingmiao <xingmiao1990@126.com>
2020-06-18 00:00:57 +0800

    获取单个或所有信息

--- 14_/routes/api/profiles.js
+++ 14_/routes/api/profiles.js
@@ -15,9 +15,8 @@ router.get("/test",(req,res) => {
 // @route POST api/profiles/add
 // @desc 创建信息接口
 // @access private（类型）
-router.post("/add",(req,res) => {
+router.post("/add",passport.authenticate('jwt',{session: false}),(req,res) => {
     const profileFields = {};
-    console.log('sdfs')
 
     if(req.body.type) profileFields.type = req.body.type;
     if(req.body.describe) profileFields.describe = req.body.describe;
@@ -30,6 +29,40 @@ router.post("/add",(req,res) => {
         res.json(profile)
     })
 
+})
+
+// @route GET api/profiles
+// @desc 获取所有信息
+// @access private（类型）
+
+ router.get("/",passport.authenticate('jwt',{session: false}),(req,res)  => {
+     console.log(req)
+     Profile.find()
+         .then(profile => {
+             if(!profile) {
+                 res.status(404).json('没有任何内容');
+             }else {
+                res.json(profile)   
+             }
+         })
+         .catch(err => res.status(404).json(err));     
+ })
+
+ // @route GET api/profiles/:id
+// @desc 获取单个信息
+// @access private（类型）
+
+router.get("/:id",passport.authenticate('jwt',{session: false}),(req,res)  => {
+    console.log(req)
+    Profile.findOne({_id:req.params.id})
+        .then(profile => {
+            if(!profile) {
+                res.status(404).json('没有任何内容');
+            }else {
+               res.json(profile)   
+            }
+        })
+        .catch(err => res.status(404).json(err));     
 })
 
 module.exports = router;
